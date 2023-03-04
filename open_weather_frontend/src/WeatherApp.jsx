5ed7236f84b1fa48e6115aa0951c45ed5ae636ab
FaithAbiola <abiolafaith92@gmail.com>
2023-03-04 14:56:55 +0100

    Frontend Task

---
+++ open_weather_frontend/src/WeatherApp.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./WeatherApp.css";
+
+function WeatherApp() {
+    const [city, setCity] = useState("");
+    const [weather, setWeather] = useState("");
+    const [error, setError] = useState("");
+
+    const parser = new DOMParser();
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await axios.get(
+                `http://localhost:8080/api/v1/weather/city/${city}`
+            );
+            const xmlDoc = parser.parseFromString(response.data, "text/xml");
+            const weatherElement = xmlDoc.getElementsByTagName("weather")[0];
+            let description =
+                weatherElement.getElementsByTagName("description")[0].textContent;
+            description = description.charAt(0).toUpperCase() + description.slice(1);
+            setWeather(description);
+            setError("");
+        } catch (error) {
+            setWeather("");
+            setError(error.response.data.message);
+        }
+    };
+
+    const handleInputChange = (event) => {
+        setCity(event.target.value);
+        setWeather("");
+    };
+
+    return (
+        <div className="container">
+            <h1>Weather App</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="city">Enter City Name:</label>
+                <input
+                    type="text"
+                    id="city"
+                    name="city"
+                    value={city}
+                    onChange={handleInputChange}
+                />
+                <button type="submit">Find</button>
+            </form>
+            <div>
+                <h1 className="weather-description">Weather Description</h1>
+                <p>
+                    {error ? (
+                        <div className="error">{error}</div>
+                    ) : (
+                        <div className="weather"><h2>{weather}</h2></div>
+                    )}
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default WeatherApp;
